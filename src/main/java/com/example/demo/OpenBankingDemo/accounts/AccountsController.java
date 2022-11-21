1db4998028957149092e64657d6e999ca77985ac
ashishbania <ashish.bania@gmail.com>
2022-11-21 00:34:06 -0500

    Added streams for filter and request params

--- src/main/java/com/example/demo/OpenBankingDemo/accounts/AccountsController.java
+++ src/main/java/com/example/demo/OpenBankingDemo/accounts/AccountsController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping(path = "/api/v1/accounts")
@@ -20,14 +22,22 @@ public class AccountsController {
         this.accountService = accountService;
     }
     @GetMapping(path= "/")
-    public List<Accounts> getAllAccounts(){
-        return accountService.getAllAccounts();
+    public List<Accounts> getAllAccounts(@RequestParam(required = false, defaultValue = "Enabled") String status){
+        return accountService.getAllAccounts().stream().filter(x->x.getStatus().equals(status)).collect(Collectors.toList());
     }
     @GetMapping(path = "/{id}")
     public Accounts getAccountById(@PathVariable("id") Integer id){
+        //return  Stream.of(accountService.getAccounts(id)).map(x-> x.getStatus().toLowerCase()).collect(Collectors.toList());
         return accountService.getAccounts(id);
     }
 
+
+//    @GetMapping(path="/{status}")
+//    public List<Accounts> getDisabledAccounts(@PathVariable("status") String status){
+//        return accountService.getAllAccounts().stream().filter(x-> x.getStatus().equals(status)).collect(Collectors.toList());
+//    }
+
+    @GetMapping()
     @PostMapping
     public ResponseEntity addAccounts(@RequestBody Accounts accounts){
         String addAccountResponse= accountService.addAccounts(accounts);
